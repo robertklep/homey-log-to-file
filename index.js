83dbeb7b952b201b188580e0b414371714f19988
Robert Klep <robert@klep.name>
2023-01-29 09:03:10 +0100

    Set correct content type/encoding

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = async (logfile = '/userdata/std.log', port = 8008, flags = 'w')
 
   // Create HTTP server that will serve the file
   http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; ; charset=utf-8' });
     createReadStream(logfile).pipe(res);
   }).listen(port);
 
